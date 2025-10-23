89a075581e90d55148b928c8392537d5c2326d1a
Ben West <b-m-s-west@outlook.com>
2025-10-23 20:01:19 +0100

    Feat: Configuration and initialisation of different connections

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
-def main():
-    print("Hello from graph-rag!")
+from email.mime import base
+from neo4j import GraphDatabase
+from qdrant_client import QdrantClient, models
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from openai import OpenAI
+from collections import defaultdict
+from neo4j_graphrag.retrievers import QdrantNeo4jRetriever
+import uuid
+import os
 
+# Load environment variables from .env file
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+# Get credentials from environment variables
+server_host = os.getenv("SERVER_HOST")
+qdrant_name = os.getenv("QDRANT_NAME")
+qdrant_port = int(os.getenv("QDRANT_PORT"))
+neo4j_name = os.getenv("NEO4J_NAME")
+neo4j_port = int(os.getenv("NEO4J_PORT"))
+neo4j_user = os.getenv("NEO4J_USER")
+neo4j_password = os.getenv("NEO4J_PASSWORD")
+
+qdrant_url = f"http://{server_host}:{qdrant_port}"
+neo4j_url = f"http://{server_host}:{neo4j_port}"
+
+# Using Ollama for locally run models
+openai_client = OpenAI(
+    base_url=f"http://{server_host}:11434/v1",
+    api_key="ollama"
+)
+
+# Initialize Neo4j driver
+neo4j_driver = GraphDatabase.driver(
+    neo4j_url,
+    auth=(neo4j_user, neo4j_password)   
+)
+
+# Initialize Qdrant client
+qdrant_client = QdrantClient(
+    url=qdrant_url
+)
